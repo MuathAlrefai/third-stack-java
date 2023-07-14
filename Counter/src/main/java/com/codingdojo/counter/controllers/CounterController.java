5e5b58b0e0de7a7af1a00e81c4bd6cf52b41221d
MuathAlrefai <rigahd@gmail.com>
2023-07-14 20:34:22 +0300

    week 2

--- Counter/src/main/java/com/codingdojo/counter/controllers/CounterController.java
+++ Counter/src/main/java/com/codingdojo/counter/controllers/CounterController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CounterController {
+    //increment visit by 1
     @RequestMapping("/")
     public String index(HttpSession session) {
         if (session.getAttribute("count") == null) {
@@ -17,6 +18,26 @@ public class CounterController {
             session.setAttribute("count", count + 1);
         }
 
+        return "index.jsp";
+    }
+
+    //increment visit by 2
+    @RequestMapping("/two")
+    public String index2(HttpSession session) {
+        if (session.getAttribute("count") == null) {
+            session.setAttribute("count", 0);
+        }
+        else {
+            int count = (int) session.getAttribute("count");
+            session.setAttribute("count", count + 2);
+        }
+
+        return "index2.jsp";
+    }
+
+    @RequestMapping("/reset")
+    public String reset(HttpSession session){
+        session.removeAttribute("count");
         return "index.jsp";
     }
 
--- Counter/src/main/webapp/WEB-INF/counter.jsp
+++ Counter/src/main/webapp/WEB-INF/counter.jsp
@@ -12,6 +12,9 @@
     <title>Counter</title>
 </head>
 <body style="text-align: center; font-family: sans-serif; background-color: lightgrey;">
-    <h2>You have visited <a href="http://localhost:8080/">This Site</a> <c:out value="${count}"/> Times</h2>
+    <h2>You have visited http://localhost:8080/ <c:out value="${count}"/> Times</h2>
+    <h2><a href="http://localhost:8080/">visit again! (+1)</a></h2>
+    <h2><a href="http://localhost:8080/two">visit again! (+2)</a></h2>
+    <h3><a href="/reset">delete the count</a></h3>
 </body>
 </html>
